5a75d6a277e6c4ecaa51eb8d02e3644d2d5cbb18
KostaJovic <95864701+KostaJovic@users.noreply.github.com>
2023-03-27 10:21:02 +0200

    Update

--- src/TiltCard.js
+++ src/TiltCard.js
@@ -8,40 +8,29 @@ function TiltCard() {
     const rect = e.target.getBoundingClientRect();
     const x = ((e.clientX - rect.left) / rect.width) * 2 - 1;
     const y = -((e.clientY - rect.top) / rect.height) * 2 + 1;
-    setTilt({ x, y });
-  };
+    requestAnimationFrame(() => setTilt({ x, y }));
+  };  
 
   const handleMouseLeave = () => {
     setTilt({ x: 0, y: 0 });
   };
 
   return (
-    <div className="relative 
-                w-52 h-52 
-                bg-white 
-                rounded-md 
-                shadow-lg 
-                overflow-hidden 
-                transition-transform 
-                transform-gpu 
-                hover:scale-105 
-                hover:rotate-y-3 
-                hover:-rotate-x-3 
-                hover:z-10 
-                content-center 
-                justify-center"
+    <div
+      className="relative w-52 h-52 bg-white rounded-md shadow-lg overflow-hidden content-center justify-center"
       style={{
         transform: `rotateY(${tilt.x * 10}deg) rotateX(${tilt.y * 10}deg) translateZ(20px)`,
         perspective: "800px",
+        willChange: "transform",
       }}
       onMouseMove={handleMouseMove}
       onMouseLeave={handleMouseLeave}
     >
+
         <div className="relative m-auto z-10 w-52 h-52">
             <img src={photo} className="absolute 
                                         m-auto 
-                                        inset-0        
-                                        transition-shadow "/>
+                                        inset-0"/>
         </div> 
     </div>
   );
